b2d40193dbf91ab69df94b841e67ae46a8f7ffaa
chiseungii <43616130+chiseungii@users.noreply.github.com>
2020-07-09 20:56:56 +0900

    Create 문자열_내_p와_y의_개수.py

---
+++ 문자열_내_p와_y의_개수.py
@@ -0,0 +1,8 @@
+def solution(s):
+    p = y = 0
+    for i in range(len(s)):
+        if s[i] == 'p' or s[i] == 'P': p += 1
+        elif s[i] == 'y' or s[i] == 'Y': y += 1
+        
+    if p == y: return True
+    else: return False
