f2cb96337cf163c27225be5c19953f37699c9b6d
chiseungii <43616130+chiseungii@users.noreply.github.com>
2020-07-10 21:46:50 +0900

    Create 콜라츠_추측.py

---
+++ 콜라츠_추측.py
@@ -0,0 +1,15 @@
+def solution(num):
+    cnt = 0
+    while num != 1:
+        if cnt == 500:
+            cnt = -1
+            break
+        cnt += 1
+        
+        if num % 2:
+            num *= 3
+            num += 1
+        else:
+            num //= 2
+            
+    return cnt
